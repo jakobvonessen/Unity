143c2649007a59a5e5fe17141e6a617d762604cf
jakobvonessen <code@jakobve.com>
2019-08-11 17:46:42 +0200

    Initial release

---
+++ _.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _ : MonoBehaviour {
+    public static void log(float m)
+    {
+        Debug.Log(m);
+    }
+    public static void log(string m)
+    {
+        Debug.Log(m);
+    }
+    public static bool i(string character)
+    {
+        if (Input.GetKeyDown(character))
+            return true;
+        return false;
+    }
+}
---
+++ f5.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class EditorShortCutKeys : ScriptableObject
+{
+
+    [MenuItem("Edit/Run _F5")] // _Shortcut to play
+    static void PlayGame()
+    {
+        if (!Application.isPlaying)
+        {
+            EditorSceneManager.SaveScene(SceneManager.GetActiveScene(), "", false);
+            EditorApplication.ExecuteMenuItem("Edit/Play");
+        }
+    }
+
+}
