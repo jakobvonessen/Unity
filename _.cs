7e104b50d67319f01fd89135c1c9da10956645ef
jakobvonessen <code@jakobve.com>
2019-08-11 17:47:47 +0200

    Current version

--- _.cs
+++ _.cs
@@ -17,4 +17,41 @@ public class _ : MonoBehaviour {
             return true;
         return false;
     }
+
+    public static Vector3 v(float x, float y, float z)
+    {
+        return new Vector3(x, y, z);
+    }
+
+    public static void toggleRenderers(int mode = -1, params GameObject[] gos)
+    {
+        foreach (GameObject go in gos)
+            toggleRenderer(go, mode);
+    }
+
+    public static void toggleRenderer(GameObject go, int mode = -1)
+    {
+        Renderer[] rs = go.GetComponentsInChildren<Renderer>();
+        if(rs.Length > 0)
+        {
+            foreach(Renderer r in rs)
+                r.enabled = (mode == 1) ? true : (mode == 0) ? false : !r.enabled;
+        } else
+        {
+            Renderer r = go.GetComponent<Renderer>();
+            r.enabled = (mode == 1) ? true : (mode == 0) ? false : !r.enabled;
+        }
+    }
+
+    public static Vector3 dPos(float i, float steps, Vector3 oPos, Vector3 diffPos, bool reverse = false)
+    {
+        if (reverse)
+            i = steps - i;
+        return oPos + diffPos * dSine(i, steps);
+    }
+
+    public static float dSine(float i, float steps)
+    {
+        return ((1 - (Mathf.Cos(i / steps * Mathf.PI))) / 2);
+    }
 }
